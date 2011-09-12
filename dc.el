f91a9611e48d61bb53accf37959909b8f070faf4
Daniel Cheah <me@dancheah.com>
2011-09-11 22:07:31 -0700

    added ensime module

--- dc.el
+++ dc.el
@@ -12,5 +12,22 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'font-lock-mode)
 
+;;; Setup scala mode
+(add-to-list 'load-path "/opt/personal/pkg/scala/misc/scala-tool-support/emacs")
+(require 'scala-mode-auto)
+
+;;; Setup ensime
+;; Load the ensime lisp code...
+(add-to-list 'load-path "~/.emacs.d/ensime/src/main/elisp")
+(require 'ensime)
+
+;; This step causes the ensime-mode to be started whenever
+;; scala-mode is started for a buffer. You may have to customize this step
+;; if you're not using the standard scala mode.
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+;; MINI HOWTO: 
+;; Open .scala file. M-x ensime (once per project)
+
 ;;; I like having a menu bar
 (menu-bar-mode t)
